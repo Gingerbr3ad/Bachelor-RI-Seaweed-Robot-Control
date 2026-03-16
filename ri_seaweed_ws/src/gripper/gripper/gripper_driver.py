98278fed97ed215ccdd91cd157d496a290b31f20
Wiktor Rutkowski <rwiktor4627@gmail.com>
2026-03-16 11:59:40 +0100

    Changed the gripper close command to act as a service rather than topic

--- ri_seaweed_ws/src/gripper/gripper/gripper_driver.py
+++ ri_seaweed_ws/src/gripper/gripper/gripper_driver.py
@@ -1,26 +1,31 @@
 
 import rclpy
 from rclpy.node import Node
+from std_srvs.srv import SetBool
 from std_msgs.msg import Bool
+from threading import Event
 
 from gpiozero import DigitalInputDevice
 from gpiozero import OutputDevice
 
 
-#This node is responsible for gripper control, it has a subscriber which listens for gripper close/open command, and it sends the HIGH for girpper lock command and LOW for gripper unlock command on listener callback.
-#It also has a publisher updates the 'gripper/state' topicon interrupts from the gripper sensor pin
+#This node is responsible for gripper control, it has a service which listens for gripper close/open request, and it sets GPIO to HIGH for gripper lock request and LOW for gripper unlock request.
+#On GPIO interrupt it publishes the new gripper state to the gripper/state topic.
+
+#TODO: Add some form of timeout and error detection 
 
 class GripperDriver(Node):
 
     def __init__(self):
         super().__init__('gripper_driver')
         self.publisher = self.create_publisher(Bool, "gripper/state", 10)
-        self.subscription = self.create_subscription(Bool, 'gripper/close_command', self.listener_callback, 10)
-        self.subscription #Avoids 'vaiable unused' warning
+        self.srv = self.create_service(SetBool, 'gripper_command', self.gripper_command_callback)
         
         self.sensor_pin = DigitalInputDevice(17, pull_up=False, bounce_time=0.1) #Sensor pin from arduino is configured to GPIO 17
         self.lock_pin = OutputDevice(4)                                          #Pin for sending gripper lock command is configured to GPIO 4
         self.lock_pin.off()
+        self.interrupt_event = Event()
+        self.gripperClosed = self.sensor_pin.value #false -> gripper open | true -> gripper closed
 
         #Interrupt callbacks for sensor pin
         self.sensor_pin.when_activated = self.gripper_open
@@ -30,23 +35,45 @@ class GripperDriver(Node):
     def gripper_closed(self):
         msg = Bool()
         msg.data = True
+        self.gripperClosed = True
+        self.interrupt_event.set()
         self.get_logger().info("Gripper state changed: CLOSED")
         self.publisher.publish(msg)
     
     def gripper_open(self):
         msg = Bool()
         msg.data = False
+        self.gripperClosed = False
+        self.interrupt_event.set()
         self.get_logger().info("Gripper state changed: OPEN")
         self.publisher.publish(msg)
-    
-    #Listener callback function sends lock/unlock signal to arduino on change in 'gripper/close_command' topic
-    def listener_callback(self, msg):
-        if(msg.data):
+
+    #Service callback function sends lock/unlock signal to arduino on service request
+    def gripper_command_callback(self, request, response):
+        self.interrupt_event.clear()
+        if(request.data):
             self.lock_pin.on()
             self.get_logger().info("Gripper command pin set: CLOSED")
-        elif (msg.data != True):
+            #Waits until the gripper state maches the requested state
+            while(self.gripperClosed == False):
+                self.interrupt_event.wait() #Wait for gripper to change state
+                self.interrupt_event.clear()
+
+            response.success = True
+            response.message = "Gripper closed"
+        
+        elif(request.data != True):
             self.lock_pin.off()
             self.get_logger().info("Gripper command pin set: OPEN")
+            #Waits until the gripper state maches the requested state
+            while(self.gripperClosed == True):
+                self.interrupt_event.wait() #Wait for gripper to change state
+                self.interrupt_event.clear()
+
+            response.success = True
+            response.message = "Gripper open"
+        
+        return response
 
 
 def main(args=None):
--- ri_seaweed_ws/src/gripper/gripper/gripper_dummy_controller.py
+++ ri_seaweed_ws/src/gripper/gripper/gripper_dummy_controller.py
@@ -1,36 +1,41 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.logging import LoggingSeverity
-from std_msgs.msg import Bool
+from std_srvs.srv import SetBool
 
 
-#This is a dummy gripper controller node, it periodically sends lock/unlock commands to the 'gripper/close_command' topic.
+#This is a dummy gripper controller node, it periodically sends lock/unlock requests to the gripper_command service.
 #The purpouse of this node is to test the gripper driver node without the rest of the system for legitimate gripper lock/unlock calls.
 
 class GripperDummyController(Node):
 
     def __init__(self):
         super().__init__('gripper_dummy_controller')
-        self.publisher_ = self.create_publisher(Bool, 'gripper/close_command', 10)
+        self.cli = self.create_client(SetBool, 'gripper_command')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        
         timer_period = 5 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        self.close_commad = False
+        self.close_command = False
 
-    #Sends the lock/unlock command on timer callback
-    rclpy.logging.set_logger_level('gripper_dummy_controller', LoggingSeverity.DEBUG)
+    #Sends the lock/unlock request on timer callback
     def timer_callback(self):
-        msg = Bool()
+        request = SetBool.Request()
 
-        if(self.close_commad == False):
-            self.close_commad = True
+        if(self.close_command == False):
+            self.close_command = True
             self.get_logger().debug("Dummy gripper command: CLOSE")
-        elif(self.close_commad == True):
-            self.close_commad = False
+        elif(self.close_command == True):
+            self.close_command = False
             self.get_logger().debug("Dummy gripper command: OPEN")
         
-        msg.data = self.close_commad
-        self.publisher_.publish(msg)
+        request.data = self.close_command
+        self.cli.call_async(request)
+    
+    #Sets the node default logging to DEBUG level
+    rclpy.logging.set_logger_level('gripper_dummy_controller', LoggingSeverity.DEBUG)
 
             
 
